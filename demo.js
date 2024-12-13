d02d9205b12d670a2ce5d1a6a7e161798fd76426
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-12-13 17:16:37 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,42 @@
+function iphone()
+{
+    console.log('this is iphone')
+}
+function samsung()
+{
+    console.log('this is samsung')
+}
+function oppo()
+{
+    console.log('this is oppo')
+}
+iphone()
+samsung()
+oppo()
+
+
+// -create two variables a and b
+// -create a function called add
+// this functions should print addition of two number
+
+var a=10
+var b=10
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+
+// create a variable called factor,fplayer,fmovie and create a function caklled fav() 
+
+var factor='thalapathy vijay'
+var fplayer='messi'
+var fmovie='leo'
+function fav()
+{
+    console.log('fav actor:',factor)
+    console.log('fav player:',fplayer)
+    console.log('fav movie:',fmovie)
+}
+fav()
